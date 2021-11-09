a9934bd3eaba198cd44ca1ced3ae2a9cd97a3d67
chris dany <chrisdany9@gmail.com>
2021-11-09 20:43:55 +0200

    generated the menu component

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -27,6 +27,7 @@ import { FooterComponent } from './layouts/footer/footer.component';
 import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
 import { ActiveMenuDirective } from './layouts/navbar/active-menu.directive';
 import { ErrorComponent } from './layouts/error/error.component';
+import { MenuComponent } from './layouts/menu/menu.component';
 
 @NgModule({
   imports: [
@@ -58,7 +59,7 @@ import { ErrorComponent } from './layouts/error/error.component';
     { provide: NgbDateAdapter, useClass: NgbDateDayjsAdapter },
     httpInterceptorProviders,
   ],
-  declarations: [MainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, ActiveMenuDirective, FooterComponent],
+  declarations: [MainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, ActiveMenuDirective, FooterComponent, MenuComponent],
   bootstrap: [MainComponent],
 })
 export class AppModule {
