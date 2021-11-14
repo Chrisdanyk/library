78a537c9097a3acfa7d75d9ae4db01516b5e87f9
chris dany <chrisdany9@gmail.com>
2021-11-15 01:56:12 +0200

    restrict access on the home page, the user needs to be authenticated

--- src/main/webapp/app/home/home.route.ts
+++ src/main/webapp/app/home/home.route.ts
@@ -1,11 +1,14 @@
 import { Route } from '@angular/router';
 
 import { HomeComponent } from './home.component';
+import { UserRouteAccessService } from '../core/auth/user-route-access.service';
 
 export const HOME_ROUTE: Route = {
   path: '',
   component: HomeComponent,
   data: {
+    authorities: ['ROLE_USER', 'ROLE_CLIENT', 'ROLE_ADMIN'],
     pageTitle: 'home.title',
   },
+  canActivate: [UserRouteAccessService],
 };
